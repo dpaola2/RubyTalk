c5c1c16635a18ca708ecb0c8a83938761a44c5d5
Dave Paola <dpaola2@gmail.com>
2014-01-20 12:51:00 -0800

    record changes in JSON format

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+require 'json'
 
 # See test.rb for usage details
 
@@ -6,6 +7,7 @@ Class.class_eval do
     block = eval("lambda {#{ proc_string }}")
     define_method name.to_sym, block
     instance_method_list[name.to_sym] = proc_string
+    record_change(self.name, name, proc_string)
     block
   end
 
@@ -15,7 +17,6 @@ Class.class_eval do
     end
     @instance_method_list
   end
-  
 end
 
 Object.class_eval do
@@ -42,4 +43,35 @@ def all_modules
     result << obj
   end
   result
+end
+
+def find_my_objects
+  results = []
+  ObjectSpace.each_object do |obj|
+    if obj.methods.index(:instance_method_list) != nil
+      results << obj
+    end
+  end
+  results
+end
+
+def record_change(class_name, method_name, proc_string)
+  filepath = "changes.json"
+  changes = {}
+  if File.exists? filepath
+    # read file, decode into hash
+    file = File.open(filepath, "r")
+    changes = JSON.parse(file.read)
+  end
+  
+  # write new values to the hash
+  if changes[class_name].nil?
+    changes[class_name] = {}
+  end
+  changes[class_name][method_name] = proc_string
+  
+  # write the file
+  File.open(filepath, "w") do |f|
+    f.write(JSON.pretty_generate(changes))
+  end
 end
