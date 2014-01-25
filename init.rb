df84b23b562b1d2237ce3a3a6895f00c856bd513
Dave Paola <dpaola2@gmail.com>
2014-01-25 12:14:12 -0800

    RubyTalk module in subdirectory

--- init.rb
+++ init.rb
@@ -1,14 +1,12 @@
 require 'bundler/cli'
 require 'json'
 require 'debugger'
-
-# See test.rb for usage details
+require './lib/ruby_talk.rb'
 
 def my_require(name)
   record_require(name)
   require name
 end
-
 
 def create_class(name, superclass)
   if superclass.nil?
@@ -17,50 +15,6 @@ def create_class(name, superclass)
   Object.const_set(name, Class.new(superclass))
   record_class_change(name, superclass.to_s)
   return eval(name)
-end
-
-Class.class_eval do
-  define_method :define_instance_method_from_string do |name, proc_string|
-    block = eval("lambda {#{ proc_string }}")
-    define_method name.to_sym, block
-    instance_method_list[name.to_sym] = proc_string
-    record_method_change(self.name, name, proc_string)
-    block
-  end
-
-  define_method :instance_method_list do
-    if @instance_method_list.nil?
-      @instance_method_list = {}
-    end
-    @instance_method_list
-  end
-
-  define_method :define_class_method_from_string do |name, proc_string|
-    block = eval("lambda {#{ proc_string }}")
-    self.class.instance_eval do
-      define_method name.to_sym, block
-    end
-    class_method_list[name.to_sym] = proc_string
-    record_class_method_change(self.name, name, proc_string)
-    block
-  end
-
-  define_method :class_method_list do
-    if @class_method_list.nil?
-      @class_method_list = {}
-    end
-    @class_method_list
-  end
-end
-
-Object.class_eval do
-  define_method :method_list do
-    self.class.instance_method_list
-  end
-
-  define_method :descendants do
-    ObjectSpace.each_object(Class).select { |klass| klass < self }
-  end
 end
 
 def all_classes
@@ -223,11 +177,16 @@ rescue NameError
   return false
 end
 
-def main
+def welcome
+  puts "Bootstrapping environment..."
+  RubyTalk.bootstrap
   puts "Loading changes from classes.json and methods.json..."
   load_changes
   puts "Done."
+  puts ""
+  puts "### WELCOME TO RUBYTALK ###"
+  puts ""
 end
 
-main()
+welcome()
   
---
+++ lib/ruby_talk.rb
@@ -0,0 +1,48 @@
+
+module RubyTalk
+  def self.bootstrap
+    Class.class_eval do
+      define_method :define_instance_method_from_string do |name, proc_string|
+        block = eval("lambda {#{ proc_string }}")
+        define_method name.to_sym, block
+        instance_method_list[name.to_sym] = proc_string
+        record_method_change(self.name, name, proc_string)
+        block
+      end
+
+      define_method :instance_method_list do
+        if @instance_method_list.nil?
+          @instance_method_list = {}
+        end
+        @instance_method_list
+      end
+
+      define_method :define_class_method_from_string do |name, proc_string|
+        block = eval("lambda {#{ proc_string }}")
+        self.class.instance_eval do
+          define_method name.to_sym, block
+        end
+        class_method_list[name.to_sym] = proc_string
+        record_class_method_change(self.name, name, proc_string)
+        block
+      end
+
+      define_method :class_method_list do
+        if @class_method_list.nil?
+          @class_method_list = {}
+        end
+        @class_method_list
+      end
+    end
+
+    Object.class_eval do
+      define_method :method_list do
+        self.class.instance_method_list
+      end
+
+      define_method :descendants do
+        ObjectSpace.each_object(Class).select { |klass| klass < self }
+      end
+    end
+  end
+end
