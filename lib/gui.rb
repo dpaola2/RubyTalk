4af1270bbc24755f736a39467cb290520a6c78ac
Dave Paola <dpaola2@gmail.com>
2014-01-28 19:06:01 -0800

    add show dock button to root tk window

--- lib/gui.rb
+++ lib/gui.rb
@@ -3,5 +3,12 @@ class RubyTalkGUI
   def initialize
     @root = TkRoot.new { title "RubyTalk" }
     @dock = RubyTalkDock.new(@root)
+    @dock_btn = TkButton.new(@root) do
+      text "Show Dock"
+      pack
+    end
+    @dock_btn.command do
+      RubyTalkDock.new(@root)
+    end
   end
 end
