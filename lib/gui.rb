989aea4b1474fa8ea4a51841012c54037a268837
Dave Paola <dpaola2@gmail.com>
2014-01-28 18:41:56 -0800

    moved gui functionality into a RubyTalkDock class

--- lib/gui.rb
+++ lib/gui.rb
@@ -1,37 +1,7 @@
 
 class RubyTalkGUI
   def initialize
-    @root = TkRoot.new { title "RubyTalkGUI" }
-    @new_workspace_btn = TkButton.new(@root) do
-      borderwidth 1
-      state "normal"
-      text "New Workspace"
-      pack
-    end
-    @new_workspace_btn.command do
-      new_workspace
-    end
-
-    @new_system_browser_btn = TkButton.new(@root) do
-      borderwidth 1
-      state "normal"
-      text "System Browser"
-      pack
-    end
-    @new_system_browser_btn.command do
-      new_system_browser
-    end
-  end
-
-  def new_workspace
-    Workspace.new(@root)
-  end
-
-  def new_window
-    TkToplevel.new(@root)
-  end
-
-  def root
-    @root
+    @root = TkRoot.new { title "RubyTalk" }
+    @dock = RubyTalkDock.new(@root)
   end
 end
